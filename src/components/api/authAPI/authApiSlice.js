e4177c8bbdc8496cf8badb7fdaa2ff854daad017
mddanish867 <mddanish867@gmail.com>
2024-12-26 20:16:45 +0530

    modified the authApiSlice

--- src/components/api/authAPI/authApiSlice.js
+++ src/components/api/authAPI/authApiSlice.js
@@ -1,3 +1,27 @@
+// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+// import { BASE_URL } from "../constant";
+
+// export const authApiSlice = createApi({
+//   reducerPath: "authApi",
+//   baseQuery: fetchBaseQuery({
+//     baseUrl: BASE_URL,
+//     credentials: "include",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },   
+//   }),
+//   endpoints: (builder) => ({
+//     registerUser: builder.mutation({
+//       query: (userData) => ({
+//         url: "auth/register",
+//         method: "POST",
+//         body: userData,
+//       }),
+//     }),
+//   }),
+// });
+
+// export const { useRegisterUserMutation } = authApiSlice;
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { BASE_URL } from "../constant";
 
@@ -6,9 +30,10 @@ export const authApiSlice = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: BASE_URL,
     credentials: "include",
-    headers: {
-      "Content-Type": "application/json",
-    },   
+    prepareHeaders: (headers) => {
+      headers.set("Content-Type", "application/json");
+      return headers;
+    },
   }),
   endpoints: (builder) => ({
     registerUser: builder.mutation({
