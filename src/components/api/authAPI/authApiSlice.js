27677ea1074e55cb741624040659017eb1329c81
mddanish867 <mddanish867@gmail.com>
2025-01-01 12:39:31 +0530

    remodified these two

--- src/components/api/authAPI/authApiSlice.js
+++ src/components/api/authAPI/authApiSlice.js
@@ -1,27 +1,3 @@
-// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-// import { BASE_URL } from "../constant";
-
-// export const authApiSlice = createApi({
-//   reducerPath: "authApi",
-//   baseQuery: fetchBaseQuery({
-//     baseUrl: BASE_URL,
-//     credentials: "include",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },   
-//   }),
-//   endpoints: (builder) => ({
-//     registerUser: builder.mutation({
-//       query: (userData) => ({
-//         url: "auth/register",
-//         method: "POST",
-//         body: userData,
-//       }),
-//     }),
-//   }),
-// });
-
-// export const { useRegisterUserMutation } = authApiSlice;
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { BASE_URL } from "../constant";
 
