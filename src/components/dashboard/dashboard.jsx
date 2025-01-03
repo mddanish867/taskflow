1eedfff8619f94afe19d3f10ecf27e7b816bacd8
mddanish867 <mddanish867@gmail.com>
2025-01-03 19:49:19 +0530

    remodified the css

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -135,7 +135,7 @@ const Dashboard = () => {
 
             <button
               onClick={handleLogOut}
-              className="flex items-center gap-20 w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 rounded-lg mt-4"
+              className="flex items-center gap-2 w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 rounded-lg mt-4"
             >
               <LogOut size={20} />
               Logout
