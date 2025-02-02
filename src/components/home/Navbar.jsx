4472cab9578c820ff5b682bc533fc63eacea59f0
mddanish867 <mddanish867@gmail.com>
2025-02-02 12:32:48 +0530

    remodif3d

--- src/components/home/Navbar.jsx
+++ src/components/home/Navbar.jsx
@@ -173,7 +173,7 @@ const Navbar = () => {
             )}
             <button
               onClick={toggleMenu}
-              className="inline-flex items-center justify-center p-2 -right-2 rounded-md text-gray-600 hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500"
+              className="inline-flex items-center justify-center p-2 -right-2 rounded-md text-gray-600 hover:text-gray-900 hover:bg-gray-100 focus:outline-none"
             >
               <span className="sr-only">Open main menu</span>
               {isMenuOpen ? (
